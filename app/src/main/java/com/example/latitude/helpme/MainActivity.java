808513200522e04d5a853e5fcda40c13fafdc440
afef <chrfff26@gmail.com>
2017-03-26 13:50:54 +0200

    creation template 1ere interface

--- app/src/main/java/com/example/latitude/helpme/MainActivity.java
+++ app/src/main/java/com/example/latitude/helpme/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.latitude.helpme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.latitude.helpme.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        final Button loginButton = (Button) findViewById(R.id.suivant);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Activité2.class);
+                startActivity(intent);
+            }
+        });
 }
+}
+
---
+++ app/src/main/java/com/example/latitude/helpme/OneFragment.java
@@ -0,0 +1,34 @@
+package com.example.latitude.helpme;
+
+/**
+ * Created by LATITUDE on 20/03/2017.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.latitude.helpme.R;
+
+
+public class OneFragment extends Fragment{
+
+    public OneFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+}
