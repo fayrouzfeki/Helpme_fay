38f0bd9c3c6b5aa0ebfbc821112c5fd656e3db8a
afef <chrfff26@gmail.com>
2017-03-31 12:35:37 +0200

    authentification par empreinte digitale

--- app/src/main/java/com/example/latitude/helpme/page_Inscri4.java
+++ app/src/main/java/com/example/latitude/helpme/page_Inscri4.java
@@ -1,7 +1,10 @@
 package com.example.latitude.helpme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class page_Inscri4 extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class page_Inscri4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page__inscri4);
+
+        final Button loginButton = (Button) findViewById(R.id.enregistrer);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(page_Inscri4.this, EmpreintActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
